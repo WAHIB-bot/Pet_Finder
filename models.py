cb4245a70e99e6d0f37801575273d5e29377a974
usama ashraf <97821444+usamasansi@users.noreply.github.com>
2025-05-22 03:18:30 +0500

    Update models.py

--- models.py
+++ models.py
@@ -12,3 +12,15 @@ class User(Base):
     last_name = Column(String, nullable=False)
     phone_number = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class LostPetReport(BaseModel):
+    pet_id: int
+    location: str
+    last_seen: datetime
+    description: str
+class FoundPetReport(BaseModel):
+    pet_id: int
+    found_location: str
+    found_date: datetime
+    description: str
